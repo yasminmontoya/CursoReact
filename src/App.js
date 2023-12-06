c31832c423deab2bfa0b527fa10124db2303e575
yasminmontoya <yaz.lorena@gmail.com>
2023-12-06 18:41:12 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
+
+const defaultTodos=[
+  {text:'Cortar cebolla',completed:true},
+  {text:'Tomar el Curso de Intro a React.js',completed:false},
+  {text:'Llorar con la Llorona',completed:false},
+  {text:'LALALALALA',completed:false},
+];
+
+function App() {
+  return (
+    <>
+    
+      <TodoCounter completed ={16} total={25}/>
+      <TodoSearch/>
+      
+      <TodoList>
+        {defaultTodos.map(todo =>(
+          <TodoItem key={todo.text} text={todo.text} completed={todo.completed}/>
+        ))}
+      </TodoList>
+
+      <CreateTodoButton />
+
+    </>
+  );
+}
+
+export default App;
