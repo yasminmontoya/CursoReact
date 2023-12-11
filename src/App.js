737bdba1e53c1df99c7adb7c960a1ce20ab051b2
yasminmontoya <yaz.lorena@gmail.com>
2023-12-11 09:54:25 -0500

    Agregar la funcionalidad del contador

--- src/App.js
+++ src/App.js
@@ -5,28 +5,47 @@ import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
 
-const defaultTodos=[
-  {text:'Cortar cebolla',completed:true},
-  {text:'Tomar el Curso de Intro a React.js',completed:false},
-  {text:'Llorar con la Llorona',completed:false},
-  {text:'LALALALALA',completed:false},
+const defaultTodos = [
+  { text: 'Cortar cebolla', completed: true },
+  { text: 'Tomar el Curso de Intro a React.js', completed: false },
+  { text: 'Llorar con la Llorona', completed: false },
+  { text: 'LALALALALA', completed: false },
+  { text: 'Usar estados derivados', completed: true },
 ];
 
 function App() {
+  const [todos, setTodos] = React.useState(defaultTodos);
+  const [searchValue, setSearchValue] = React.useState('');
+
+  const completedTodos = todos.filter(
+    todo => !!todo.completed
+  ).length;
+  const totalTodos = todos.length;
+
+  console.log('Los usuarios buscan todos de ' + searchValue);
+
   return (
     <>
-    
-      <TodoCounter completed ={16} total={25}/>
-      <TodoSearch/>
-      
+      <TodoCounter
+        completed={completedTodos}
+        total={totalTodos}
+      />
+      <TodoSearch
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
+
       <TodoList>
-        {defaultTodos.map(todo =>(
-          <TodoItem key={todo.text} text={todo.text} completed={todo.completed}/>
+        {defaultTodos.map(todo => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+          />
         ))}
       </TodoList>
 
       <CreateTodoButton />
-
     </>
   );
 }
